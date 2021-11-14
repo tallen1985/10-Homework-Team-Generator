d1be07b87716a5a5409f1682e40e6e263d87d6ca
Troy Allen <jake.allen@me.com>
2021-11-14 06:58:15 -0500

    styling complete

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
 const inquirer = require('inquirer')
-const fs = require('fs')
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
-const questions = require('./src/questions')
+const questions = require('./src/questions');
+const {writeToFile} = require('./src/writeToFile')
+
 
 let team = [];
 let addTeamMember = '';
 
+let newMember = () => {
+    if (addTeamMember === 'Intern') {
+        inquirer.prompt(questions.intern).then(internAnswers => {
+        const {name, employeeID, email, school, addAnother} = internAnswers
+        const intern = new Intern(name, employeeID, email, school)
+        team.push(intern);
+        addTeamMember = addAnother;
+    }).then(() => {
+        newMember()
+    })
+    } else if (addTeamMember === 'Engineer') {
+        inquirer.prompt(questions.engineer).then(engineerAnswers => {
+            const {name, employeeID, email, gitHub, addAnother} = engineerAnswers
+            const engineer = new Engineer(name, employeeID, email, gitHub)
+            team.push(engineer);
+            addTeamMember = addAnother;
+        }).then(() => {
+            newMember()
+        })
+    } else {
+        writeToFile(team);
+    }
+
+}
+
+
 inquirer.prompt(questions.manager)
 
     .then(managerAnswers => {
-        const {name, employeeID, email, addAnother} = managerAnswers
-        const manager = new Manager(name, employeeID, email)
+        const {name, employeeID, email, officeNumber, addAnother} = managerAnswers
+        const manager = new Manager(name, employeeID, email, officeNumber)
         team.push(manager)
         addTeamMember = addAnother;
     }).then(() => {
-        if (addTeamMember === 'Intern') {
-                inquirer.prompt(questions.intern).then(internAnswers => {
-                const {name, employeeID, email, school, addAnother} = internAnswers
-                const intern = new Intern(name, employeeID, email, school)
-                team.push(intern);
-                addTeamMember = addAnother;
-            }).then(() => {
-                console.log(team)
-            })
-        } else if (addTeamMember === 'Engineer') {
-            inquirer.prompt(questions.engineer).then(engineerAnswers => {
-                const {name, employeeID, email, gitHub, addAnother} = engineerAnswers
-                const engineer = new Intern(name, employeeID, email, gitHub)
-                team.push(engineer);
-                addTeamMember = addAnother;
-            }).then(() => {
-                console.log(team)
-            })
-        }
+        newMember()
     })
